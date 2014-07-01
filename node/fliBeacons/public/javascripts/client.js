b3e968e0a713b0610421b43991639dd0d9d27c7f
marcbaechinger <marc.baechinger@gmail.com>
2014-07-01 11:25:02 +0200

    fixed id bug

--- node/fliBeacons/public/javascripts/client.js
+++ node/fliBeacons/public/javascripts/client.js
@@ -2,10 +2,13 @@
 	
 	var log = document.querySelector("#log"),
 		stations = document.querySelector("#stations"),
+		createId = function (id) {
+			return "a" + id.replace(" ", "-");
+		},
 		addDrone = function (drone) {
 			var el = document.createElement("div"),
 				buf = "<label>Drone</label>",
-				id = station.id.replace(" ", "-");
+				id = createId(drone.id);
 				
 				
 			buf += "<pre>" + JSON.stringify(drone, null, 2) + "</pre>";
@@ -14,13 +17,13 @@
 			el.classList.add("panel");
 			log.appendChild(el);
 			console.log(drone);
-			var stationEl = document.getElementById(drone.id);
+			var stationEl = document.getElementById(createId(drone.id));
 			stationEl.addDrone(drone);
 			
 		},
 		updateBaseStations = function (baseStations) {
 			baseStations.forEach(function (station) {
-				var id = station.id.replace(" ", "-");
+				var id = createId(station.id);
 				if (!document.querySelector("#" + id)) {	
 					var el = document.createElement("fli-base-station");
 					el.name = station.name;
@@ -32,7 +35,7 @@
 			});
 		},
 		removeBaseStation = function (baseStation) {
-			var id = station.name.replace(" ", "-"),
+			var id = createId(baseStation.id),
 				el = document.querySelector("#" + id);
 			if (el) {	
 				el.parentNode.removeChild(el);
