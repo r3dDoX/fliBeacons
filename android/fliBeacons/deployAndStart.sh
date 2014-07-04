ff4ba3b7832b130f8d3a001a1992acc7bd9afe39
Benjamin Müller <bm@fork.ch>
2014-07-04 10:10:45 +0200

    uninstalls the app first

--- android/fliBeacons/deployAndStart.sh
+++ android/fliBeacons/deployAndStart.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 gradle assembleDebug 
+adbplus shell pm uninstall -k ch.fork.flibeacons
+#adbplus uninstall -k ch.fork.flibeacons 
 adbplus install -r app/build/outputs/apk/app-debug.apk 
 adbplus shell am start -n ch.fork.flibeacons/.activities.StartActivity
